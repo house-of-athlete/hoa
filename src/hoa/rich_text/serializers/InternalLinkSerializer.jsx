0e47aff6bd92d9c8325450d5b5e411bdf8c3670c
Brian Alexander <balexand@gmail.com>
2021-11-18 12:12:42 -0700

    Shared top nav (#22)
    * comments
    
    * Update InternalLinkSerializer.jsx
    
    * `CMSLink` config from context
    
    * Update InternalLinkSerializer.jsx

--- src/hoa/rich_text/serializers/InternalLinkSerializer.jsx
+++ src/hoa/rich_text/serializers/InternalLinkSerializer.jsx
@@ -3,9 +3,11 @@ import React from "react"
 import { CMSLink } from "../../links"
 
 export const InternalLinkSerializer = ({ children, mark }) => {
-  const [LinkComponent, props] = CMSLink.getInternalLink(mark.doc)
+  // CMSLink expects `document` field, not `doc`. We might want to rename these for consistency.
+  const { doc, ...link } = mark
+  link.document = doc
 
-  return <LinkComponent {...props}>{children}</LinkComponent>
+  return <CMSLink link={link}>{children}</CMSLink>
 }
 
 InternalLinkSerializer.propTypes = {
