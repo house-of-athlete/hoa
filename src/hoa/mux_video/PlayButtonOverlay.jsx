ff90f366edf8c3b06afe91f9f361b58ff217ee0f
Brian Alexander <balexand@gmail.com>
2021-11-23 14:02:04 -0700

    Update PlayButtonOverlay.jsx

--- src/hoa/mux_video/PlayButtonOverlay.jsx
+++ src/hoa/mux_video/PlayButtonOverlay.jsx
@@ -1,15 +1,24 @@
 import React from "react"
+import styled from "@emotion/styled"
+
+const StyledButton = styled.div`
+  --ar-h: 9;
+  --ar-w: 16;
+  --border-radius: 15%;
+
+  aspect-ratio: var(--ar-w) / var(--ar-h);
+  border-radius: var(--border-radius) /
+    calc(var(--border-radius) * var(--ar-w) / var(--ar-h));
+  width: 30%;
+`
 
 export const PlayButtonOverlay = () => (
   <div className="absolute flex items-center inset-0 justify-center pointer-events-none">
-    <div
-      className="bg-opacity-25 bg-white flex h-32 items-center justify-center w-40"
-      style={{ borderRadius: "30px" }}
-    >
+    <StyledButton className="bg-opacity-25 bg-white flex items-center justify-center">
       <svg className="w-6" viewBox="30.93000030517578 0 148.1400146484375 210">
         <path d="M179.07,105L30.93,210V0L179.07,105z" fill="white" />
       </svg>
-    </div>
+    </StyledButton>
   </div>
 )
 
