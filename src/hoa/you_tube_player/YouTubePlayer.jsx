8b7403d3c31a3c68cdc9025a80ef63ae48ab7eb6
Brian Alexander <balexand@gmail.com>
2021-11-30 14:09:56 -0700

    YouTubePlayer

---
+++ src/hoa/you_tube_player/YouTubePlayer.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "@emotion/styled"
+
+const Styled = styled.div`
+  padding-bottom: 56.25%; // 16:9 aspect ratio
+  position: relative;
+  width: 100%;
+
+  > iframe {
+    left: 0;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    width: 100%;
+  }
+`
+
+export const YouTubePlayer = ({ id }) => (
+  <Styled>
+    <iframe
+      src={`https://www.youtube.com/embed/${id}?autoplay=0&playsinline=1&modestbranding=1`}
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </Styled>
+)
+
+YouTubePlayer.propTypes = {
+  id: PropTypes.string.isRequired,
+}
