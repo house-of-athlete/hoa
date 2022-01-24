2b702b856ebf2f8303a860ff9a659324a5f1170f
Brian Alexander <balexand@gmail.com>
2022-01-24 08:59:45 -0700

    fix for #52

--- src/hoa/klaviyo/KlaviyoForm.jsx
+++ src/hoa/klaviyo/KlaviyoForm.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 // https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=<id> must be loaded by GTM
-export const KlaviyoFormPageComponent = ({ klaviyoClass }) => (
+export const KlaviyoForm = ({ klaviyoClass }) => (
   <div className={klaviyoClass} />
 )
 
-KlaviyoFormPageComponent.propTypes = {
+KlaviyoForm.propTypes = {
   klaviyoClass: PropTypes.string.isRequired,
 }
